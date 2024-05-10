06ff34a9a4caca87943c3d9b00004686ad327185
atuluttam <atuluttam150998@gmail.com>
2024-05-10 20:47:30 +0530

    remove-temporary-files

--- cpp_components/include/interface.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef ALGO_HPP
-#define ALGO_HPP
-
-#include "/home/atuluttam/work/git-remake/code-dump-cmake-code/common_expt/check-conflict-serializability-using-precedence-graph/cpp_components/libs/data_operations/include/data_operations.hpp"
-
-#endif  // ALGO_HPP
