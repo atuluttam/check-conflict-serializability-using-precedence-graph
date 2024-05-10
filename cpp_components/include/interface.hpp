da1659fea68295c3a5efe6e307b62ed1ebc0bbbf
atuluttam <atuluttam150998@gmail.com>
2024-05-10 20:39:39 +0530

    source-code

---
+++ cpp_components/include/interface.hpp
@@ -0,0 +1,6 @@
+#ifndef ALGO_HPP
+#define ALGO_HPP
+
+#include "/home/atuluttam/work/git-remake/code-dump-cmake-code/common_expt/check-conflict-serializability-using-precedence-graph/cpp_components/libs/data_operations/include/data_operations.hpp"
+
+#endif  // ALGO_HPP
